674616d0aa783840128dd1eb6a6ef45854469324
Kartik Sharma <160398376+KartikDevSharma@users.noreply.github.com>
2024-06-17 18:35:18 +0530

    Add files via upload

---
+++ tic tac toe/script.js
@@ -0,0 +1,105 @@
+const X_CLASS = 'x';
+const O_CLASS = 'o';
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+const winningMessageElement = document.getElementById('winningMessage');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+const restartButton = document.getElementById('restartButton');
+const xWinsElement = document.getElementById('xWins');
+const oWinsElement = document.getElementById('oWins');
+let xWins = 0;
+let oWins = 0;
+let oTurn;
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
+
+function startGame() {
+    oTurn = false;
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(O_CLASS);
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    setBoardHoverClass();
+    winningMessageElement.classList.remove('show');
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = oTurn ? O_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false);
+        updateWinCount(currentClass);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+        setBoardHoverClass();
+    }
+}
+
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.innerText = "Draw!";
+    } else {
+        winningMessageTextElement.innerText = `${oTurn ? "O's" : "X's"} Wins!`;
+    }
+    winningMessageElement.classList.add('show');
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
+    });
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    oTurn = !oTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(O_CLASS);
+    if (oTurn) {
+        board.classList.add(O_CLASS);
+    } else {
+        board.classList.add(X_CLASS);
+    }
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass);
+        });
+    });
+}
+
+function updateWinCount(winningClass) {
+    if (winningClass === X_CLASS) {
+        xWins++;
+        xWinsElement.innerText = xWins;
+    } else if (winningClass === O_CLASS) {
+        oWins++;
+        oWinsElement.innerText = oWins;
+    }
+}
